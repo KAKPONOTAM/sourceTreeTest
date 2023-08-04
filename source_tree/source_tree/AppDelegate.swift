063b9592340bad616329d5c2824581defa7fe7d1
KAKPONOTAM <sabitovdanill@icloud.com>
2023-08-04 16:16:57 +0300

    SourceTreeTest-2: Add another documentation

--- source_tree/source_tree/AppDelegate.swift
+++ source_tree/source_tree/AppDelegate.swift
@@ -4,6 +4,7 @@ import UIKit
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     //documentation is added
     //another documentation
+    //third-documentation 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
