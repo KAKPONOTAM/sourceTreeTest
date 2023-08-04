56299dcc72e1b733d3be5fccdc94e39b36e22e58
KAKPONOTAM <sabitovdanill@icloud.com>
2023-08-04 15:31:31 +0300

    SourceTreeTest-1: remove no need code

--- source_tree/source_tree/ViewController.swift
+++ source_tree/source_tree/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  source_tree
-//
-//  Created by Сабитов Данил on 03.08.2023.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
